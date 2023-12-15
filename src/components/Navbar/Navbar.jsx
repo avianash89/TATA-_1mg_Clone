9778b82e719c6a00c4935798e0e90a33ca5832e2
Avinash Prasad <avinashprasad4608@gmail.com>
2023-12-15 23:41:23 +0530

    change footer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -565,7 +565,9 @@ export default function Navbar() {
           </ul>
         </div>
       </div>
-      <div className="footer4"></div>
+      <div className="footer4">
+        
+      </div>
     </div>
   );
 }
